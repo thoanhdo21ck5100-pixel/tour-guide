2da1a5ba4024279ec7c624c700331e80d14b5a88
thoanhdo21ck5100-pixel <thoanhdo21ck5100@gmail.com>
2026-09-08 19:22:08 +0700

    perf(core-web-vitals): remove font preload chaining, optimize browserslist and lucide-react imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const notoSansJP = Noto_Sans_JP({
   weight: ['400', '700'],
   display: 'swap',
   variable: '--font-noto-sans-jp',
-  preload: true,
+  preload: false,
 });
 
 export const metadata: Metadata = constructMetadata();
